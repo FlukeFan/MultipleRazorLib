707a56cea39f800236e8947f82c896fbcbceabb1
Richard Brown <fluke_fan@hotmail.com>
2018-10-02 13:57:46 +0100

    Added home controller that provides links to the 2 library controller actions.

---
+++ Web/Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultipleRazorLib.Web
+{
+    [Route("/")]
+    public class Controller : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content(@"
+                <a href='/lib1'>lib1</a>
+                <br/>
+                <a href='/lib2'>lib2</a>
+            ", "text/html");
+        }
+    }
+}
